832b2ef4962d888ed9250ec2077185f39bcc6b7e
Eric <erictraccitto@gmail.com>
2024-04-26 18:45:51 -0400

    Completed the html layout and the checkPalindrome function that is used to check if the number inputted by user is a palindrome. Also used the Math.floor function to drop the decimal.

---
+++ Question3/ex3.js
@@ -0,0 +1,20 @@
+function checkPalindrome() {
+    const input = document.getElementById("input");
+    const output = document.getElementById("result");
+
+    const reg = new RegExp(/^\d+(\.\d+)?$/);
+
+    if (isNaN(input.value) || !reg.test(input.value)) {
+        alert("Try again: Please enter a valid positive number.")
+        output.textContent = "";
+    } else {
+        let reverse = 0;
+        let temp = input.value;
+        for (let i = 0; temp > 0; i++) {
+            reverse = (reverse * 10) + (temp % 10);
+            temp = Math.floor(temp / 10);
+        }
+
+        output.textContent = "Is the number a palindrome? Answer: " + (input.value == reverse ? "yes" : "no");
+    }
+}
