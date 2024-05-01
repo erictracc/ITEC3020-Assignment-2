08da89a3cd3ad8e6f5138a29fddcd7b1c58777ca
Eric <erictraccitto@gmail.com>
2024-04-30 22:36:05 -0400

    Minor fixes and code readability for question 3.

--- Question3/ex3.js
+++ Question3/ex3.js
@@ -1,29 +1,29 @@
 //A function to check if a number is a palindrome or not
 function checkPalindrome() {
     //Gets the input element and the output element from the HTML document
-    const input = document.getElementById("input-number");
-    const output = document.getElementById("result");
+    const inputValue = document.getElementById("input_num");
+    const outputValue = document.getElementById("final_result");
 
     //Regular expression to validate input as a positive number
     const reg = new RegExp(/^\d+(\.\d+)?$/);
 
     //Checks if the input value is not a valid positive number
-    if (isNaN(input.value) || !reg.test(input.value)) {
+    if (isNaN(inputValue.value) || !reg.test(inputValue.value)) {
         // Alert the user to enter a valid positive number
         alert("Try again: Please enter a valid positive number.")
         // Clear the output text content
-        output.textContent = "";
+        outputValue.textContent = "";
     } else {
         //Initializes variables to reverse the input number
-        let reverse = 0;
-        let temp = input.value;
+        let reverseNum = 0;
+        let temp = inputValue.value;
         // Loop to reverse the input number
         for (let i = 0; temp > 0; i++) {
-            reverse = (reverse * 10) + (temp % 10);
+            reverseNum = (reverseNum * 10) + (temp % 10);
             temp = Math.floor(temp / 10);
         }
 
         //Displays the result if the input number is a palindrome or not
-        output.textContent = "Is the number a palindrome? Answer: " + (input.value == reverse ? "yes" : "no");
+        outputValue.textContent = "Is the number a palindrome? Final Answer: " + (inputValue.value == reverseNum ? "yes it is!" : "no it isn't!");
     }
 }
