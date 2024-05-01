d17015d9dba78833a8d863dd243d84974788cb7d
Eric <erictraccitto@gmail.com>
2024-04-30 22:56:52 -0400

    Improving code readability for exercise 4.

--- Question4/ex4.js
+++ Question4/ex4.js
@@ -1,30 +1,30 @@
 // Wait for the DOM content to load before executing the script
 document.addEventListener("DOMContentLoaded", function() {
     // Select the container and generate button
-    const container = document.querySelector(".custom-container");
-    const generateButton = document.querySelector(".generate-button");
+    const custom_container = document.querySelector(".custom-container");
+    const generate_button = document.querySelector(".generate-button");
 
     // Add event listener to the generate button
-    generateButton.addEventListener("click", generateBoxes);
+    generate_button.addEventListener("click", createSquares);
 
     // Event listener for mouseover on container
-    container.addEventListener('mouseover', function(event) {
-        const box = event.target;
+    custom_container.addEventListener('mouseover', function(event) {
+        const square = event.target;
 
         // Check if the mouseover target is a box
-        if (box.classList.contains("custom-box")) {
+        if (square.classList.contains("custom-square")) {
             // Remove the box
-            box.remove();
+            square.remove();
 
             // Check if container has no child elements
-            if (container.childElementCount === 0) {
-                alert("Last Box!");
+            if (custom_container.childElementCount === 0) {
+                alert("Last Square Box!");
             }
         }
     });
 
     // Generate initial boxes
-    generateBoxes();
+    createSquares();
 });
 
 
@@ -44,18 +44,18 @@ function getRandomColour() {
 
 
 // Function to generate boxes
-function generateBoxes() {
+function createSquares() {
     const container = document.querySelector(".custom-container");
 
     // Generate 100 boxes
     for (let i = 0; i < 100; i++) {
-        const box = document.createElement("div");
-        box.classList.add("custom-box");
-        box.style.backgroundColor = getRandomColour();
-        box.style.border = "thin solid black";
-        box.style.position = "absolute";
-        box.style.top = (Math.floor(Math.random() * 400)) + "px";
-        box.style.left = (Math.floor(Math.random() * 400)) + "px";
-        container.appendChild(box);
+        const squares = document.createElement("div");
+        squares.classList.add("custom-square");
+        squares.style.backgroundColor = getRandomColour();
+        squares.style.border = "thin solid black";
+        squares.style.position = "absolute";
+        squares.style.top = (Math.floor(Math.random() * 400)) + "px";
+        squares.style.left = (Math.floor(Math.random() * 400)) + "px";
+        container.appendChild(squares);
     }
 }
